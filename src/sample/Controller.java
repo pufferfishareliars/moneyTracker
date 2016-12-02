d6941d4cc93acb5a2818a8933aee15a14a69653a
elie <vogel.elie@gmail.com>
2016-12-02 16:06:19 +0000

    Working example of using CSS and using a Controller for logic

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,18 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class Controller {
     @FXML
+    Text resultMessage, transactionOutput;
+    @FXML
+    TextField amount, category;
+
+    @FXML
     protected void handleSubmitButtonAction(ActionEvent event) {
-        return;
+        transactionOutput.setText(category.getText() + " - $" + amount.getText());
+        resultMessage.setText("Transaction added");
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,10 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import sample.Models.Transaction;
-import sample.Views.TransactionForm;
 
 public class Main extends Application {
     // TODO - how to make this responsive to window changes?
@@ -33,7 +16,9 @@ public class Main extends Application {
         //Scene scene = new Scene(transactionForm.build(), 400, 300);
         Parent root = FXMLLoader.load(getClass().getResource("transaction_form.fxml"));
         Scene scene = new Scene(root, 400, 300);
-        scene.getStylesheets().add("sample/assets/styles.css");
+        scene.getStylesheets().add(
+                this.getClass().getClassLoader().getResource("sample/assets/transaction_form_styles.css").toString()
+        );
 
         window.setScene(scene);
         window.show();
